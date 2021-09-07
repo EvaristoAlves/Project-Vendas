3b8f385786eea89f823495cd1b49c584432e1ac3
Evaristo Alves <evaristocarvalho34@gmail.com>
2021-09-06 23:25:14 -0300

    Basic static components

---
+++ frontend/src/components/footer/index.tsx
@@ -0,0 +1,15 @@
+
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/EvaristoAlves" target="_blank" rel="noreferrer">Evaristo Alves</a></p>
+                <p className="text-light"><small><strong>AppVendas com Spring React</strong><br />
+                    Projeto controle de vendas <a href="https://www.linkedin.com/in/evaristo-alves-de-carvalho-a5b7a457/" target="_blank" rel="noreferrer">@EvaristoAlves</a></small></p>
+            </div>
+        </footer>
+
+    );
+}
+
+export default Footer;
